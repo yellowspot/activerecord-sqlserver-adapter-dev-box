e9ace677be65a035d929a998e03dfbc4dc991d1c
Aidan Haran <aidanharan@yahoo.com>
2023-08-03 17:05:39 +0100

    Revert password change

--- bootstrap_sqlserver.sh
+++ bootstrap_sqlserver.sh
@@ -37,7 +37,7 @@ sleep 5
 CREATE DATABASE [activerecord_unittest];
 CREATE DATABASE [activerecord_unittest2];
 GO
-CREATE LOGIN [rails] WITH PASSWORD = 'password', CHECK_POLICY = OFF, DEFAULT_DATABASE = [activerecord_unittest];
+CREATE LOGIN [rails] WITH PASSWORD = '', CHECK_POLICY = OFF, DEFAULT_DATABASE = [activerecord_unittest];
 GO
 USE [activerecord_unittest];
 CREATE USER [rails] FOR LOGIN [rails];
